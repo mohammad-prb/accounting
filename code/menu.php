e06bb91869ee56c2311e12c9c2496f6774864aa8
Mohammad <pspipm27@gmail.com>
2022-01-05 13:27:24 +0330

    menu

--- code/menu.php
+++ code/menu.php
@@ -3,7 +3,10 @@
     <h1 id="titrHDR">حسابداری</h1>
     <div class="kadrMenu">
         <ul class="menu">
-            <li><a class="itemMNU" href="index.php"<?php echo ($safheh == "index" ? " data-enekhabi" : "");?>><span class="iconItemMNU"></span><span class="matnItemMNU">حساب ها</span></a></li>
+            <li><a class="itemMNU" href="index.php"<?php echo ($safheh == "index" ? " data-enekhabi" : "");?>><span class="iconItemMNU"></span><span class="matnItemMNU">صفحه اصلی</span></a></li>
+            <li><a class="itemMNU" href="soorathesab.php"<?php echo ($safheh == "soorathesab" ? " data-enekhabi" : "");?>><span class="iconItemMNU"></span><span class="matnItemMNU">صورتحساب</span></a></li>
+            <li><a class="itemMNU" href="amar.php"<?php echo ($safheh == "amar" ? " data-enekhabi" : "");?>><span class="iconItemMNU"></span><span class="matnItemMNU">آمار</span></a></li>
+            <li><a class="itemMNU" href="tanzimat.php"<?php echo ($safheh == "tanzimat" ? " data-enekhabi" : "");?>><span class="iconItemMNU"></span><span class="matnItemMNU">تنظیمات</span></a></li>
         </ul>
     </div>
 </div>
